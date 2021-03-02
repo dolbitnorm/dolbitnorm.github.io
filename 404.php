fa5770d206e5c913b9f7d04f04b13f27cf5e2d9a
dolbitnorm <57843288+dolbitnorm@users.noreply.github.com>
2021-03-02 13:33:39 +0500

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>masquarade: Здесь ничего нет...</title>
+    <link rel="stylesheet" href="css/news-piece-empty.css">
+    <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon">
+    <meta name="description" content="masquarade: Акции и скидки на обучение в школе">
+</head>
+
+<body>
+    <header class="container">
+        <div class="logo">
+            <a href="index.html">
+                <div class="logo-img">
+                    <img src="img/logo.png" alt="">
+                    <h1>masquarade</h1>
+                </div>
+            </a>
+        </div>
+        <div class="nav">
+            <h1 class="title"><a href="news.html">Новости</a></h1>
+            <a href="index.html" class="main">На главную</a>
+        </div>
+    </header>
+    <h1 class="error">Упс..</h1>
+    <h2 class="error">Здесь ничего нет, но скоро будет, обещаю</h2>
+</body>
+
+</html>
